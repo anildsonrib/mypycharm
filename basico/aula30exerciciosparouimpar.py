90e9c373af722804e917342f800614fabd807b82
anildsonrib <anildson.ri@gmail.com>
2022-04-06 22:16:42 -0300

    emport

---
+++ basico/aula30exerciciosparouimpar.py
@@ -0,0 +1,20 @@
+"""
+Faça um programa que peça ao usuário para digitar um número inteiro;
+Informe se este número é par ou ímpar;
+Caso o usuário não digite um número inteiro, informe que não é um número inteiro.
+
+
+num = input('Digite um número inteiro: ')
+
+if num.isdigit():
+
+    num = int(num)
+
+    if num % 2 == 0:
+        print('Número par.')
+    else:
+        print('Número ímpar.')
+
+else:
+    print('Você não digitou um número inteiro.')
+"""
