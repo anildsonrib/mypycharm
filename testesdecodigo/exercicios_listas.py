210ebd3d20ecece4498fa3ab51a800c78d0c3e47
anildsonrib <anildson.ri@gmail.com>
2022-04-24 21:01:37 -0300

    exercicios

---
+++ testesdecodigo/exercicios_listas.py
@@ -0,0 +1,96 @@
+# 1 - Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+
+
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+# 2 - Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+# 3 - Faça um Programa que leia 4 notas, mostre as notas e a média na tela.
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+# 4 - Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+"""
+5 - Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os números pares no vetor PAR e
+os números IMPARES no vetor impar. Imprima os três vetores.
+"""
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+"""
+6 - Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno, 
+imprima o número de alunos com média maior ou igual a 7.0.
+"""
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+# 7 - Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números.
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+"""
+8 - Faça um Programa que peça a idade e a altura de 5 pessoas, armazene cada informação no seu respectivo vetor. 
+Imprima a idade e a altura na ordem inversa a ordem lida.
+"""
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+"""
+9 - Faça um Programa que leia dois vetores com 10 elementos cada. Gere um terceiro vetor de 20 elementos, cujos 
+valores deverão ser compostos pelos elementos intercalados dos dois outros vetores.
+"""
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+# 10 - Altere o programa anterior, intercalando 3 vetores de 10 elementos cada.
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+"""
+11 - Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine quantos alunos com mais de 
+13 anos possuem altura inferior à média de altura desses alunos.
+
+"""
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+"""
+12 - Faça um programa que receba a temperatura média de cada mês do ano e armazene-as em uma lista. 
+Após isto, calcule a média anual das temperaturas e mostre todas as temperaturas acima da média anual,
+e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ).
+
+"""
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+"""
+# 13 - Utilizando listas faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
+- "Telefonou para a vítima?"
+- "Esteve no local do crime?"
+- "Mora perto da vítima?"
+- "Devia para a vítima?"
+-"Já trabalhou com a vítima?" O programa deve no final emitir uma classificação sobre a participação da pessoa no crime. Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".
+
+"""
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+"""
+14 - Faça um programa que leia um número indeterminado de valores, correspondentes a notas, encerrando a entrada de dados quando for informado um valor igual a -1 (que não deve ser armazenado). Após esta entrada de dados, faça:
+# Mostre a quantidade de valores que foram lidos;
+# Exiba todos os valores na ordem em que foram informados, um ao lado do outro;
+# Exiba todos os valores na ordem inversa à que foram informados, um abaixo do outro;
+# Calcule e mostre a soma dos valores;
+# Calcule e mostre a média dos valores;
+# Calcule e mostre a quantidade de valores acima da média calculada;
+# Calcule e mostre a quantidade de valores abaixo de sete;
+# Encerre o programa com uma mensagem;
+
+"""
+
+
+# print('Olá, mundo!')
