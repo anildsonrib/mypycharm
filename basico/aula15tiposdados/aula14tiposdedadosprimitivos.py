0669213ab5f9cfcb33adfae1ce48d8b2781d5f26
anildsonrib <anildson.ri@gmail.com>
2022-06-24 23:38:54 -0300

    24 06 sab

---
+++ basico/aula15tiposdados/aula14tiposdedadosprimitivos.py
@@ -0,0 +1,105 @@
+"""
+
+
+                                  AULA 15 - Tipos de dados "primitivos"
+
+------------------------------------------------------------------------------------------------------------------------
+* String:
+          Na programação String representa um conjunto de caracteres disposto numa determinada ordem. A partir de agora,
+          todas as vezes em que falarmos o termo String, estaremos nos referindo a um conjunto de caracteres.
+
+------------------------------------------------------------------------------------------------------------------------
+* Inteiro:
+           Um segundo tipo de informação são os dados compostos por caracteres numéricos (algarismo). É considerado um
+           número inteiro, qualquer algarismo positivo, negativo ou zero (...e que não possua ponto)
+
+------------------------------------------------------------------------------------------------------------------------
+Float (ponto flutuante):
+                         No python não utilizamos virgula em algarismos. E dizemos que um número é 'float' ou de 'ponto
+                         flutuante' quando ele tem casas decimais, ex: 8.6, 3.652, 0.0
+
+------------------------------------------------------------------------------------------------------------------------
+* Bool:
+        Em Python o tipo bool especifica os valores booleanos falso (False) e verdadeiro (True). Podemos criar variáveis
+        associadas a booleanos, mas o uso mais comum é na verificação de resultados de expressões relacionais e lógicas.
+        Ex: 10 == 10 => True
+            20 < 15 => False
+------------------------------------------------------------------------------------------------------------------------
+* Type:
+        O nome da função é type e ela exibe O TIPO de um valor ou variável. O valor ou variável, que é chamado de
+        argumento da função, tem que vir entre parênteses. É comum se dizer que uma função 'recebe' um valor ou mais
+        valores e 'retorna' um resultado. O resultado é chamado de valor de retorno.
+Ex:
+print(type('verdura')) # Neste caso, o dado a ser classificado deve vir dentro de parênteses.
+print(type(10))
+print(type(3.5))
+print(type(False))
+
+R: <class 'str'>  # class (classificação do dado) ///'str' (tipo do dado)
+   <class 'int'>
+   <class 'float'>
+   <class 'bool'>
+
+------------------------------------------------------------------------------------------------------------------------
+
+
+"""
+
+
+#======================================================================================================================
+#======================================================================================================================
+
+# Type:
+
+# Utilizando a função 'type', mostre no console a classificação dos seguinte dados à seguir:
+# 'verdura, 10, -12,5, False'
+
+print(type('verdura'))
+print(type(10))
+print(type(-12.5))
+print(type(False))
+
+# R: <class 'str'>
+#    <class 'int'>
+#    <class 'float'>
+#    <class 'bool'>
+
+
+#======================================================================================================================
+
+
+
+
+#======================================================================================================================
+
+
+
+
+#======================================================================================================================
+
+
+
+
+
+#======================================================================================================================
+
+
+
+
+#======================================================================================================================
+
+
+
+
+#======================================================================================================================
+
+
+
+
+#======================================================================================================================
+
+
+
+
+
+#======================================================================================================================
